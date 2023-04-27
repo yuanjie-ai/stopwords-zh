63e0e451ad32c460ada436686ad742d10e686541
betterme <betterme@bettermedeMacBook-Pro.local>
2023-04-27 17:55:34 +0800

    init

--- git_init.sh
+++ git_init.sh
@@ -4,7 +4,7 @@ git init
 git add *
 git commit -m "init"
 
-git remote add origin git@github.com:yuanjie-ai/stopwords-zh.git
-git branch -M master
+#git remote add origin git@github.com:yuanjie-ai/stopwords-zh.git
+#git branch -M master
 git push -u origin master
 # git remote remove origin
